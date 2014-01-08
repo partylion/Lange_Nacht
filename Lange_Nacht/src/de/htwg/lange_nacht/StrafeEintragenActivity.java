fe24433ef685ae16ee7bc7705f002873c2b1ebe6
Patrick Staiger <partylion1@web.de>
2014-01-08 20:34:08 +0100

    Dummy-Daten in Spinner eingefügt

--- Lange_Nacht/src/de/htwg/lange_nacht/StrafeEintragenActivity.java
+++ Lange_Nacht/src/de/htwg/lange_nacht/StrafeEintragenActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -16,6 +17,7 @@ public class StrafeEintragenActivity extends Activity {
 	private TextView txtViewPreis;
 	private Spinner spinnerSpieler;
 	private Spinner spinnerStrafen;
+	private String[] stringArray = { "one", "two", "three", "four", "five" };
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,12 @@ public class StrafeEintragenActivity extends Activity {
 		txtViewPreis = (TextView) findViewById(R.id.txtViewPreis);
 		spinnerSpieler = (Spinner) findViewById(R.id.spinnerSpieler);
 		spinnerStrafen = (Spinner) findViewById(R.id.spinnerStrafen);
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, stringArray);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinnerStrafen.setAdapter(adapter);
 		
 		//Ändert den Inhalt von txtViewPreis entsprechend der ausgewählten Strafe
 		spinnerStrafen.setOnItemSelectedListener(new OnItemSelectedListener() {
